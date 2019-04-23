8662e52ad492b1a7444db71c906dc4a5c8742f0c
minhnt303 <minhnt303@gmail.com>
2019-04-23 19:52:29 +0700

    commit

--- hackathonweb19/src/components/NavBar/NavBar.jsx
+++ hackathonweb19/src/components/NavBar/NavBar.jsx
@@ -3,19 +3,21 @@ import logo from '../../logo.png'
 import '../../App.css';
 const NavBar = (props) => {
     return (
+
         <table>
             <tbody>
                 <tr className="NavBar">
                     <td className="logotd">
-                        <span className="logoName"><img src={logo} alt="weima-logo" className='classLogo' />Weima</span>
+                        <a href='http://localhost:3000' className='homepagelink'>
+                            <span className="logoName"><img src={logo} alt="weima-logo" className='classLogo' />Weima</span>
+                        </a>
                     </td>
                     <td>
                         <div className="user">login register</div>
                     </td>
                 </tr>
             </tbody>
-        </table>
-
+        </table >
     )
 }
 
--- hackathonweb19/src/components/NavBar/NavBar2.jsx
+++ hackathonweb19/src/components/NavBar/NavBar2.jsx
@@ -11,7 +11,9 @@ const NavBar = (props) => {
             <tbody>
                 <tr className="NavBar2">
                     <td className="logotd">
-                        <span className="logoName"><img src={logo} alt="weima-logo" className='classLogo' />Weima</span>
+                        <a href='http://localhost:3000' className='homepagelink'>
+                            <span className="logoName"><img src={logo} alt="weima-logo" className='classLogo' />Weima</span>
+                        </a>
                     </td>
                     <td style={{ textAlign: 'center' }}>
                         <div className="Search">
@@ -23,7 +25,7 @@ const NavBar = (props) => {
 
                     <td style={{ textAlign: 'right', paddingBottom: '10px' }}>
                         <a href="http://localhost:3000/createpost"><img src={portlogo} alt="weima-userlogo" className='classuserLogo' /></a>
-                        <a href="http://localhost:3000/profile"><img src={userlogo} alt="weima-userlogo" className='classuserLogo' style={{marginLeft: "20px"}}/></a>
+                        <a href="http://localhost:3000/profile"><img src={userlogo} alt="weima-userlogo" className='classuserLogo' style={{ marginLeft: "20px" }} /></a>
                     </td>
                 </tr>
             </tbody>
